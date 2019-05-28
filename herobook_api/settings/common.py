c7ae79ccb35afa992acfe7a943f8aadccf67fadc
Protosac <protosacq@gmail.com>
2019-05-27 18:59:28 -0700

    Fix CORS package errors, mgmt cmds and update settings

--- herobook_api/settings/common.py
+++ herobook_api/settings/common.py
@@ -39,13 +39,13 @@ DEBUG = False
 # List of hosts allowed to access the app
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8000',
-    'localhost:3000',
-    'localhost:4200',
-    '127.0.0.1:8000',
-    '127.0.0.1:3000',
-    'localhost:8080',
-    '127.0.0.1:8080',
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://localhost:4200',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 )
 
 # =======================================================================
@@ -70,8 +70,8 @@ EXTERNAL_APPS = [
 ]
 
 INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS
-
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,8 +79,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 # =======================================================================
 # CONFIGURATION SETTINGS
