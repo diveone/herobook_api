5c6b0302a45a6f6b59d6f2bc86c1f495c803a55c
Protosac <protosacq@gmail.com>
2019-05-27 17:36:33 -0700

    Update configuration

--- herobook_api/wsgi.py
+++ herobook_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herobook_api.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herobook_api.settings.common')
 
 application = get_wsgi_application()
