59a9803fba13ce74e488d8275be4dd7895c44dac
Protosac <protosacq@gmail.com>
2019-05-27 18:12:12 -0700

    Add homepage redirect and cleanup packages

--- herobook_api/urls.py
+++ herobook_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(('api_urls', 'api'))),
+    path('', RedirectView.as_view(pattern_name='api:heroes:list'))
 ]
